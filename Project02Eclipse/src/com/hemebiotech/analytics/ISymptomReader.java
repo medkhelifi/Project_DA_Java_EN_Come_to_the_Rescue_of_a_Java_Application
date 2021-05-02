fc762d9ee307bdb069b8015f2cbeb2b275274f44
FixGia <fx.giracca@gmail.com>
2021-05-02 23:11:22 +0200

    Implementation IsymptomReader.
    AnalyticsCounter isn't the main app anymore, MainApp is.

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,13 +1,26 @@
 package com.hemebiotech.analytics;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+
+
+
+
+
 
 /**
  * Anything that will read symptom data from a source
  * The important part is, the return value from the operation, which is a list of strings,
- * that may contain many duplications
+ * that may contain many duplications.
+ * Count symptom's occurrences.
+ * Order the list.
  * 
- * The implementation does not need to order the list
+ *
+ * @param 
  * 
  */
 public interface ISymptomReader {
@@ -16,5 +29,18 @@ public interface ISymptomReader {
 	 * 
 	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	List<String> GetSymptoms ();
-}
+	ArrayList<String> GetSymptoms ();
+	
+	/**@return a raw listing of all Symptoms obtained from a data source with symptom's occurrences */
+	
+	Map<String, Long> CountSymptomsOccurences ();
+	
+	/** @return a raw listing of all Symptoms obtained from a data source order by alphabetical order */
+	
+	TreeMap<String,Long> NaturalOrder();
+	
+	
+
+	
+	
+	}
--- Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -2,9 +2,19 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+
+
+
 
 /**
  * Simple brute force implementation
@@ -12,20 +22,24 @@ import java.util.List;
  */
 public class ReadSymptomDataFromFile implements ISymptomReader {
 
+
 	private String filepath;
+
 	
+
+		
+
 	/**
 	 * 
 	 * @param filepath a full or partial path to file with symptom strings in it, one per line
 	 */
 	public ReadSymptomDataFromFile (String filepath) {
 		this.filepath = filepath;
-	}
+		}
 	
 	@Override
-	public List<String> GetSymptoms() {
+	public ArrayList<String> GetSymptoms() {
 		ArrayList<String> result = new ArrayList<String>();
-		
 		if (filepath != null) {
 			try {
 				BufferedReader reader = new BufferedReader (new FileReader(filepath));
@@ -34,14 +48,62 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
 				while (line != null) {
 					result.add(line);
 					line = reader.readLine();
+					
 				}
 				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
-		}
+			
+			catch (IOException e) {
+				e.printStackTrace();	
+			}
+	}
 		
 		return result;
 	}
+	
+
+
+	@Override
+	public  Map<String, Long> CountSymptomsOccurences() {
+		
+		Map<String,Long> countSymptomsOccurences=
+			
+		
+	GetSymptoms().stream().collect(Collectors.groupingByConcurrent(e->e, Collectors.counting()));
+		
+
+		
+
+	return countSymptomsOccurences;
+	
+
+
+	
+	}
+
+	@Override
+	public TreeMap<String, Long> NaturalOrder() {
+	
+		TreeMap<String, Long > naturalOrder = new TreeMap<String, Long>(CountSymptomsOccurences());
+		
+		return naturalOrder;
+	}
+
 
 }
+
+
+		
+		
+			
+	
+
+
+
+		
+	
+
+	
+
+
+	
